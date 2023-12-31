ee60c42266e2c2031644a1570ff108196de67013
Maciej B <mrdyrt.inf@gmail.com>
2023-12-31 13:36:29 +0100

    Adding Enum constructor and double color examples

---
+++ src/DoubleColon.java
@@ -0,0 +1,43 @@
+
+//in another one there was :: and why doesn't the call g(f::something) work and probably with inheritance
+
+    @FunctionalInterface
+    interface MyFunctionalInterface {
+        void something();
+    }
+
+    // FirstClass class
+    class FirstClass {
+        public static void doSomething() {
+            System.out.println("FirstClass is doing something.");
+        }
+    }
+
+    // SecondClass class extending FirstClass
+    class SecondClass extends FirstClass {
+        /*public void g(MyFunctionalInterface f) {
+            f.something();
+        }*/
+        public void g(MyFunctionalInterface f) {
+            f.something();
+        }
+    }
+
+    // Example usage
+    public class DoubleColon {
+        public static void main(String[] args) {
+            //was
+            //g(Fisrtclass::doSomething);
+
+            // Creating an instance of SecondClass
+            SecondClass secondInstance = new SecondClass();
+
+            // Using the g method with a method reference to FirstClass's doSomething method
+            secondInstance.g(FirstClass::doSomething);
+
+            // Using the g method with a lambda expression
+            secondInstance.g(() -> System.out.println("Lambda expression is doing something."));
+
+
+        }
+    }
---
+++ src/EnumConstructor.java
@@ -0,0 +1,32 @@
+enum Size {
+
+    // enum constants calling the enum constructors
+    SMALL(20,"The size is small."),
+    MEDIUM(30,"The size is medium."),
+    LARGE(40,"The size is large."),
+    EXTRALARGE(50,"The size is extra large.");
+
+    private final String pizzaSize;
+    private final int size;
+
+    // private enum constructor
+    private Size(int size, String pizzaSize) {
+        this.pizzaSize = pizzaSize;
+        this.size = size;
+    }
+
+    public String getSize() {
+        return pizzaSize;
+    }
+    public int getSizeCm()
+    {
+        return size;
+    }
+
+}
+class EnumConstructor {
+    public static void main(String[] args) {
+        Size size = Size.SMALL;
+        System.out.println(size.getSize()+" " + size.getSizeCm());
+    }
+}
