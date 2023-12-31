b4116b91c93d66b03521cf55aaf1d4cbfcac90a5
Maciej B <mrdyrt.inf@gmail.com>
2023-12-31 12:54:00 +0100

    Adding another examples

---
+++ src/LsClass.java
@@ -0,0 +1,58 @@
+// in another one there was a constructor for inheritance and why it doesn't work in the base one there was an int in constructor(int cos) but not in the derivative (classes l1111 and l11111)
+//
+
+// L1111 class
+class L1111 {
+    private int number;
+
+    public L1111(int number) {
+        this.number = number;
+    }
+
+    /*public int getNumber() {
+        return number;
+    }
+
+    public void displayInfo() {
+        System.out.println("L1111 Number: " + number);
+    }*/
+}
+
+// L11111 class extending L1111
+class L11111 extends L1111 {
+    private String description;
+
+    public L11111(int number, String description) {
+        super(number);
+        this.description = description;
+    }
+    //why this code don't work
+    /*public L11111() {
+
+    }*/
+
+
+    //unnecessary code
+   /* public String getDescription() {
+        return description;
+    }
+
+    // Override displayInfo() to include L11111-specific information
+    @Override
+    public void displayInfo() {
+        super.displayInfo(); // Call the displayInfo() method of the parent class
+
+        System.out.println("L11111 Description: " + description);
+    }*/
+}
+
+// Example usage
+public class LsClass {
+    public static void main(String[] argsda) {
+        // Creating an instance of the L11111 class
+        L11111 l11111Instance = new L11111(123, "Sample Description");
+
+        // Displaying information using the overridden displayInfo() method
+        //l11111Instance.displayInfo();
+    }
+}
---
+++ src/Mov.java
@@ -0,0 +1,69 @@
+
+// including nuances with classes and checking if the code is correct empty constructors etc. etc. etc.
+
+    // Cinema class
+    class Cinema {
+        private String name;
+        private int numberOfScreens;
+
+        public Cinema() {
+
+        }
+        public Cinema(String name, int numberOfScreens) {
+            this.name = name;
+            this.numberOfScreens = numberOfScreens;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getNumberOfScreens() {
+            return numberOfScreens;
+        }
+
+        public void displayInfo() {
+            System.out.println("Cinema Name: " + name);
+            System.out.println("Number of Screens: " + numberOfScreens);
+        }
+    }
+
+    // Movie class extending Cinema
+    class Movie extends Cinema {
+        private String title;
+        private int durationMinutes;
+
+        public Movie(String cinemaName, int numberOfScreens, String title, int durationMinutes) {
+            super(cinemaName, numberOfScreens);
+            this.title = title;
+            this.durationMinutes = durationMinutes;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public int getDurationMinutes() {
+            return durationMinutes;
+        }
+
+        // Override displayInfo() to include movie-specific information
+        @Override
+        public void displayInfo() {
+            super.displayInfo(); // Call the displayInfo() method of the parent class
+
+            System.out.println("Movie Title: " + title);
+            System.out.println("Duration: " + durationMinutes + " minutes");
+        }
+    }
+
+    // Example usage
+    public class Mov {
+        public static void main(String[] args) {
+            // Creating an instance of the Movie class
+            Cinema movie = new Movie("Example Cinema", 3, "Sample Movie", 120);
+
+            // Displaying information using the overridden displayInfo() method
+            movie.displayInfo();
+        }
+    }
